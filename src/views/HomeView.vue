a9d5c7d1316143f2766f394c02ea926da1e06422
Minh Nguyen <minhnguyen5913@gmail.com>
2025-11-13 22:05:19 -0500

    Added sample sticker grids

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup lang="ts">
 import CoreNavbar from '@/components/CoreNavbar.vue';
+import StickerGridItem from '@/components/StickerGridItem.vue';
 </script>
 
 <template>
   <main>
     <CoreNavbar />
+    <div class = "more-grids">
+      <StickerGridItem text = "sample text"/>
+      <StickerGridItem text="sample text2" />
+      <StickerGridItem text="sample text3" />
+      <StickerGridItem text="sample text4" />
+    </div>
   </main>
 </template>
+
+<style scoped>
+  .more-grids {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    width: 50vw;
+    gap: 32px;
+  }
+
+</style>
+
+
