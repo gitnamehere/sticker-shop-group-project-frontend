8c8f5192cb4a43a722af742b6e51e76bd047b28e
gitnamehere <105529595+gitnamehere@users.noreply.github.com>
2025-11-26 21:04:32 -0500

    add material picker

--- src/views/StickerView.vue
+++ src/views/StickerView.vue
@@ -9,10 +9,30 @@ const route = useRoute()
 
 const stickerName = ref('')
 const description = ref('')
+const creator = ref('A Creator')
 const stickerType = ref('')
 const stickerData = ref('')
 const stickerShape = ref('square')
+const selectedMaterial = ref('');
 const color = ref('red')
+
+// colors and materials that are currently in the db
+// TODO: replace these with an api call
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black', 'white']
+const materials = [
+  {
+    name: "vinyl-glossy",
+    price: 1
+  },
+  {
+    name: "vinyl-matte",
+    price: 0.75
+  },
+  {
+    name: "vinyl-holographic",
+    price: 1.25
+  },
+]
 
 onBeforeMount(async () => {
   const sticker_id = route.params.id
@@ -39,7 +59,7 @@ onBeforeMount(async () => {
 
 <template>
   <CoreNavbar />
-  <div class="container">
+  <div class="container d-flex">
     <div class="sticker-container">
       <StickerImage
         :sticker-type="stickerType"
@@ -48,23 +68,34 @@ onBeforeMount(async () => {
         :color="color"
       />
     </div>
-    <div>
-      <h2 class="mb-10">{{ stickerName }}</h2>
+    <div class="card container-fluid">
+      <h2 class="mb-2">{{ stickerName }}</h2>
       <p>{{ description }}</p>
+      <p class="fw-light">By: {{ creator }}</p>
+      <div class="mb-2">
+        <p class="mb-0">Material</p>
+        <div>
+          <button
+            v-for="material in materials"
+            :key="material.name"
+            class="btn border me-2"
+            :class="selectedMaterial === material.name ? 'btn-secondary' : 'btn-light'"
+            @click="selectedMaterial = material.name"
+          >
+            {{ material.name }}
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container {
-  display: flex;
-  flex-direction: row;
-}
-
 .sticker-container {
   display: flex;
   justify-content: center;
   align-items: center;
+  min-width: 40%;
   width: 40%;
   margin-right: 16px;
   border: 1px solid gray;
