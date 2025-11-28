3d5249c0c47d8decb4feec4153915d27935d253e
gitnamehere <105529595+gitnamehere@users.noreply.github.com>
2025-11-27 19:03:41 -0500

    add cart button to navbar

--- src/components/CoreNavbar.vue
+++ src/components/CoreNavbar.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <div class="container navbar navbar-expand-lg">
-    <h2 class="navbar__title">Sticker Shop</h2>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
+  <div class="navbar navbar-expand-lg">
+    <div class="container">
+      <div class="d-flex align-items-center">
+        <h2 class="navbar__title">Sticker Shop</h2>
+        <RouterLink to="/" class="nav-link fs-4">Home</RouterLink>
+      </div>
+      <div>
+        <RouterLink to="/cart" class="nav-link fs-4">
+          <FontAwesomeIcon icon="cart-shopping"/>
+        </RouterLink>
+      </div>
+    </div>
   </div>
 </template>
 
