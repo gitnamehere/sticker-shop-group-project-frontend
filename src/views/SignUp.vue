822bb9c1271e57d498268e974a5262b6160c3687
gitnamehere <105529595+gitnamehere@users.noreply.github.com>
2025-11-30 13:47:10 -0500

    update signup page layout

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -54,68 +54,60 @@ const submitButton = async () => {
 </script>
 
 <template>
-  <main class="signup-page">
-    <CoreNavbar />
-    <div class="container card p-4 signup-card">
-      <h2 class="mb-3">Create an account</h2>
-
-      <form @submit.prevent="submitButton">
-        <div class="row g-2">
-          <div class="col-12 col-md-4">
-            <label class="form-label">First Name</label>
-            <input v-model="firstName" placeholder="First Name" class="form-control" />
-          </div>
-          <div class="col-12 col-md-4">
-            <label class="form-label">Middle Name</label>
-            <input v-model="middleName" placeholder="Middle Name" class="form-control" />
-          </div>
-          <div class="col-12 col-md-4">
-            <label class="form-label">Last Name</label>
-            <input v-model="lastName" placeholder="Last Name" class="form-control" />
-          </div>
-        </div>
-
-        <div class="row mt-3 g-2">
-          <div class="col-12 col-md-6">
-            <label class="form-label">Email</label>
-            <input v-model="email" placeholder="you@example.com" class="form-control" />
-          </div>
-
-          <div class="col-12 col-md-6">
-            <label class="form-label">Password</label>
-            <input v-model="password" type="password" placeholder="Create a password" class="form-control" />
-          </div>
-        </div>
-
-        <div class="row mt-3 g-2">
-          <div class="col-12 col-md-6">
-            <label class="form-label">Phone Number</label>
-            <input v-model="phoneNumber" placeholder="(555) 123-4567" class="form-control" />
-          </div>
-
-          <div class="col-12 col-md-6">
-            <label class="form-label">Street</label>
-            <input v-model="street" placeholder="Street" class="form-control" />
-          </div>
-        </div>
-
-        <div class="row mt-3 g-2">
-          <div class="col-12 col-md-6">
-            <label class="form-label">City</label>
-            <input v-model="city" placeholder="City" class="form-control" />
-          </div>
-          <div class="col-12 col-md-6">
-            <label class="form-label">Postal Code</label>
-            <input v-model="postalCode" placeholder="Postal Code" class="form-control" />
-          </div>
-        </div>
-
-        <div class="d-flex justify-content-end mt-4">
-          <button type="submit" class="btn btn-primary btn-submit">Create account</button>
-        </div>
-      </form>
+  <CoreNavbar />
+  <div class="container card p-4">
+    <h2 class="mb-3">Create an account</h2>
+    <div class="row mb-4 gx-4">
+      <h4>Name</h4>
+      <div class="col-12 col-md-4">
+        <label class="form-label">First</label>
+        <input v-model="firstName" placeholder="First Name" class="form-control" />
+      </div>
+      <div class="col-12 col-md-4">
+        <label class="form-label">Middle</label>
+        <input v-model="middleName" placeholder="Middle Name" class="form-control" />
+      </div>
+      <div class="col-12 col-md-4">
+        <label class="form-label">Last</label>
+        <input v-model="lastName" placeholder="Last Name" class="form-control" />
+      </div>
     </div>
-  </main>
+    <div class="row mb-4 gx-4">
+      <h4>Account</h4>
+      <div class="col-12 col-md-6">
+        <label class="form-label">Email</label>
+        <input v-model="email" placeholder="you@example.com" class="form-control" />
+      </div>
+      <div class="col-12 col-md-6">
+        <label class="form-label">Password</label>
+        <input v-model="password" type="password" placeholder="Create a password" class="form-control" />
+      </div>
+    </div>
+    <div class="row mb-2 gx-4">
+      <h4>Contact Info</h4>
+      <div class="col-12 col-md-6">
+        <label class="form-label">Phone Number</label>
+        <input v-model="phoneNumber" placeholder="(555) 123-4567" class="form-control" />
+      </div>
+      <div class="col-12 col-md-6">
+        <label class="form-label">Street</label>
+        <input v-model="street" placeholder="Street" class="form-control" />
+      </div>
+    </div>
+    <div class="row mb-4 gx-4">
+      <div class="col-12 col-md-6">
+        <label class="form-label">City</label>
+        <input v-model="city" placeholder="City" class="form-control" />
+      </div>
+      <div class="col-12 col-md-6">
+        <label class="form-label">Postal Code</label>
+        <input v-model="postalCode" placeholder="Postal Code" class="form-control" />
+      </div>
+    </div>
+    <div class="d-flex justify-content-end">
+      <button @click="submitButton" class="btn btn-primary">Create account</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
