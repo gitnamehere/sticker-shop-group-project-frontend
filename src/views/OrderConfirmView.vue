eedef98e4bfa0b40a402ff0b0fada0065232dbad
Minh Nguyen <minhnguyen5913@gmail.com>
2025-12-01 09:42:49 -0500

    Added specific order deletion

--- src/views/OrderConfirmView.vue
+++ src/views/OrderConfirmView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter} from 'vue-router';
 import { API_URL } from '@/config';
 
 const route = useRoute();
+const router = useRouter();
 const orderId = route.params.orderId;
 
 const order = ref();
@@ -18,6 +19,28 @@ async function loadOrder() {
     order.value = await res.json();
   } catch {
     console.log("Failed to load order");
+  }
+}
+
+async function deleteOrder() {
+  if (!order.value) return;           
+  const id = order.value.order_id;        
+
+  if (!confirm('Delete this order?')) return;
+
+  try {
+    const res = await fetch(`${API_URL}orders/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (res.status === 204) {
+      alert('Order deleted!');
+      router.push('/orders/all');    
+    } else {
+      alert('Failed to delete order');
+    }
+  } catch {
+    console.error("Error deleting order");
   }
 }
 
@@ -39,6 +62,9 @@ onMounted(loadOrder);
           Size ID: {{ item.sticker_size_id }}
         </li>
       </ul>
+      <button class="btn btn-danger mt-3" @click="deleteOrder(order.orderId)">
+        Delete Order
+      </button>
     </div>
     <p v-else class="text-danger">Failed to load order</p>
   </div>
